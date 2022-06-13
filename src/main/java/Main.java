c3a4ce1ed199039ea0603e023dedf403ea6b131b
ivan89898 <chizhevskiy.ivan@mail.ru>
2022-06-13 14:00:20 +0500

    dz

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
         public int calculate ( int initialNumber, int finalNumber){
             SQRService service = new SQRService();
-            initialNumber = 25;
-            finalNumber = 1200;
-            int x = 30;
+
+            int x = 0;
+            x = x + 1;
 
             for (int i = 10; i <= 99; i++) {if (i * i >= initialNumber && i * i <= finalNumber) {
                 x = x + 1;
--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,9 +2,9 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int calcSqr(int initialNumber, int finalNumber) {
-        int x = 30;
-        initialNumber = 25;
-        finalNumber = 1200;
+        int x = 0;
+        x = x + 1;
+
         for (int i = 10; i <= 99; i++) {
             if (i * i >= initialNumber && i * i <= finalNumber) {
                 x = x + 1;
